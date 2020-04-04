efb455aecc6f7b812da36479e4d33519a70a20b6
MrNaif2018 <chuff184@gmail.com>
2020-04-04 20:40:03 +0300

    Added phone input and sms text field

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,6 +38,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~/plugins/vue-tel-input', mode: 'client' }
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ plugins/vue-tel-input.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import { install } from 'vue-tel-input-vuetify'
+
+install(Vue, { vuetify: Vuetify })
